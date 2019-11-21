b03403c06a9e5102024a7e01af5818fe739e3362
2643277978 <2643277978@qq.com>
2019-11-21 15:37:17 +0800

    1121分享

--- static/js/publicShare.js
+++ static/js/publicShare.js
@@ -366,77 +366,95 @@ $(function() {
 
 	//app之微信分享
 
-	// plus.share.getServices(function(s) {
-	// 	shares = ss;
-	// 	for (var i in ss ) {
-	// 		var s = ss[i];
-	// 		var item = document.createElement("li");
-	// 		item.setAttribute("class", "ditem");
-	// 		item.setAttribute("onclick", (s.id == "weixin") ? "shareWeiXin(this.plusShare)" : "shareAction(this.plusShare)");
-	// 		item.innerText = s.description;
-	// 		item.plusShare = s;
-	// 		list.appendChild(item);
-	// 	}
-	// }, function(e) {
-	// 	alert("获取分享服务列表失败：" + e.message);
-	// });
-	// function shareAction(s, ex) {
-	// 	outSet("分享操作：");
-	// 	if (!s) {
-	// 		outLine("无效的分享服务！");
-	// 		return;
-	// 	}
-	// 	if (s.authenticated) {
-	// 		outLine("---已授权---");
-	// 		shareMessage(s, ex);
-	// 	} else {
-	// 		outLine("---未授权---");
-	// 		s.authorize(function() {
-	// 			shareMessage(s, ex);
-	// 		}, function(e) {
-	// 			outLine("认证授权失败：" + e.code + " - " + e.message);
-	// 		});
-	// 	}
-	// }
-	// function shareMessage(s,ex){
-	// 	var msg={content:sharecontent.value,extra:{scene:ex}};
-	// 	if(pic&&pic.realUrl){
-	// 		msg.pictures=[pic.realUrl];
-	// 	}
-	//
-	// 	s.send( msg, function(){
-	// 		alert( "分享到\""+s.description+"\"成功！ " );
-	// 	}, function(e){
-	// 		alert( "分享到\""+s.description+"\"失败: "+e.code+" - "+e.message );
-	// 	} );
-	// }
-	//  function cancelAuth(){try{
-	//  	for ( var i in shares ) {
-	// 		var s = shares[i];
-	// 		if ( s.authenticated ) {
-	// 			outLine( "取消\""+s.description+"\"");
-	// 		}
-	// 		s.forbid();
-	// 	}
-    // // 取消授权后需要更新服务列表
-	// 	updateServices();
-	// 	outLine( "操作成功！" );}catch(e){alert(e);}
-	// }
-
-	document.getElementById("HN_button_tweixin").addEventListener("click", function() {
-		if(navigator.userAgent.indexOf("Html5Plus") > -1) {
-			//5+ 原生分享
-			window.plusShare({
-				title: wxconfig.title,//应用名字
-				content: wxconfig.description,
-				href:wxconfig.link ,//分享出去后，点击跳转地址
-				thumbs:wxconfig.imgUrl //分享缩略图
-			}, function(result) {
-				//分享回调
-				alert("分享成功");
-			});
-		} else {
-			//原有wap分享实现
+      document.addEventListener('plusready',function () {  
+		  //获取分享服务列表
+       plus.share.getServices(function(s) {  
+       shares = ss;  
+       for (var i in ss ) {  
+           var s = ss[i];  
+           var item = document.createElement("li");  
+           item.setAttribute("class", "ditem");  
+           item.setAttribute("onclick", (s.id == "weixin") ? "shareWeiXin(this.plusShare)" : "shareAction(this.plusShare)");  
+           item.innerText = s.description;  
+           item.plusShare = s;  
+           list.appendChild(item);  
+       }  
+       }, function(e) {  
+       alert("获取分享服务列表失败：" + e.message);  
+       });
+		
+		//是否授权
+		function shareAction(s, ex) {  
+		outSet("分享操作：");  
+		if (!s) {  
+		    outLine("无效的分享服务！");  
+		    return;  
+		}  
+		if (s.authenticated) {  
+		    outLine("---已授权---");  
+		    shareMessage(s, ex);  
+		} else {  
+		    outLine("---未授权---");  
+		    s.authorize(function() {  
+		        shareMessage(s, ex);  
+		    }, function(e) {
+		        outLine("认证授权失败：" + e.code + " - " + e.message);  
+		    });  
+		}  
 		}
-	});
+		
+		//分享内容
+		function shareMessage(s,ex){  
+		var msg={
+			title:wxconfig.title,
+			content:wxconfig.description,
+			herf:wxconfig.link,
+			pictures:wxconfig.imgUrl,
+			extra:{scene:ex},
+			};  
+		// if(pic&&pic.realUrl){  
+		//     msg.pictures=[pic.realUrl];  
+		// }  
+		s.send( msg, function(){  
+		    alert( "分享到\""+s.description+"\"成功！ " );  
+		}, function(e){  
+		    alert( "分享到\""+s.description+"\"失败: "+e.code+" - "+e.message );  
+		} );  
+		}
+		//分享按钮点击事件
+		
+	 
+	 //取消分享
+	 function cancelAuth(){try{  
+	 
+	 for ( var i in shares ) {  
+	     var s = shares[i];  
+	     if ( s.authenticated ) {  
+	         outLine( "取消\""+s.description+"\"");  
+	     }  
+	     s.forbid();  
+	 }  
+	 // 取消授权后需要更新服务列表  
+	 updateServices();  
+	 outLine( "操作成功！" );}catch(e){alert(e);}  
+	 }
+	 
+	 });// ready end
+
+	// document.getElementById("share").addEventListener("click", function() {
+	// 	if(navigator.userAgent.indexOf("Html5Plus") > -1) {
+	// 		//5+ 原生分享
+	// 		window.plusShare({
+	// 			title: wxconfig.title,//应用名字
+	// 			content: wxconfig.description,
+	// 			href:wxconfig.link ,//分享出去后，点击跳转地址
+	// 			thumbs:wxconfig.imgUrl //分享缩略图
+	// 		}, function(result) {
+	// 			//分享回调
+	// 			alert("分享成功");
+	// 		});
+	// 	} else {
+	// 		//原有wap分享实现
+	// 	}
+	// });// click end
 });
