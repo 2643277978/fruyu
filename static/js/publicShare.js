be85fbb6cb608925a4daf632563510637ff669a2
2643277978 <2643277978@qq.com>
2019-11-21 17:28:30 +0800

    fx

--- static/js/publicShare.js
+++ static/js/publicShare.js
@@ -100,7 +100,7 @@ $(function() {
 		'<img src="' + staticPath + 'images/HN_Public_sharezhi.png"alt=""></div></div></div>';
 
 	// $("head").append('<style id="publit_shear_load">.HN_PublicShare_shearBox,.HN_PublicShare_zhiyin {display:none;}</style>');
-	$("body").append(shareHtml);
+	// $("body").append(shareHtml);
 // <button open-type="share" class="HN_button_tweixin" ></button>
 
 	var device = navigator.userAgent;
@@ -366,80 +366,80 @@ $(function() {
 
 	//app之微信分享
 
-      document.addEventListener('plusready',function () {  
-		  //获取分享服务列表
-       plus.share.getServices(function(s) {  
-       shares = ss;  
-       for (var i in ss ) {  
-           var s = ss[i];  
-           var item = document.createElement("li");  
-           item.setAttribute("class", "ditem");  
-           item.setAttribute("onclick", (s.id == "weixin") ? "shareWeiXin(this.plusShare)" : "shareAction(this.plusShare)");  
-           item.innerText = s.description;  
-           item.plusShare = s;  
-           list.appendChild(item);  
-       }  
-       }, function(e) {  
-       alert("获取分享服务列表失败：" + e.message);  
-       });
-		
-		//是否授权
-		function shareAction(s, ex) {  
-		outSet("分享操作：");  
-		if (!s) {  
-		    outLine("无效的分享服务！");  
-		    return;  
-		}  
-		if (s.authenticated) {  
-		    outLine("---已授权---");  
-		    shareMessage(s, ex);  
-		} else {  
-		    outLine("---未授权---");  
-		    s.authorize(function() {  
-		        shareMessage(s, ex);  
-		    }, function(e) {
-		        outLine("认证授权失败：" + e.code + " - " + e.message);  
-		    });  
-		}  
-		}
-		
-		//分享内容
-		function shareMessage(s,ex){  
-		var msg={
-			title:wxconfig.title,
-			content:wxconfig.description,
-			herf:wxconfig.link,
-			pictures:wxconfig.imgUrl,
-			extra:{scene:ex},
-			};  
-		// if(pic&&pic.realUrl){  
-		//     msg.pictures=[pic.realUrl];  
-		// }  
-		s.send( msg, function(){  
-		    alert( "分享到\""+s.description+"\"成功！ " );  
-		}, function(e){  
-		    alert( "分享到\""+s.description+"\"失败: "+e.code+" - "+e.message );  
-		} );  
-		}
-		//分享按钮点击事件
-		
+     //  document.addEventListener('plusready',function () {
+		//   //获取分享服务列表
+     //   plus.share.getServices(function(s) {
+     //   shares = ss;
+     //   for (var i in ss ) {
+     //       var s = ss[i];
+     //       var item = document.createElement("li");
+     //       item.setAttribute("class", "ditem");
+     //       item.setAttribute("onclick", (s.id == "weixin") ? "shareWeiXin(this.plusShare)" : "shareAction(this.plusShare)");
+     //       item.innerText = s.description;
+     //       item.plusShare = s;
+     //       list.appendChild(item);
+     //   }
+     //   }, function(e) {
+     //   alert("获取分享服务列表失败：" + e.message);
+     //   });
+		//
+		// //是否授权
+		// function shareAction(s, ex) {
+		// outSet("分享操作：");
+		// if (!s) {
+		//     outLine("无效的分享服务！");
+		//     return;
+		// }
+		// if (s.authenticated) {
+		//     outLine("---已授权---");
+		//     shareMessage(s, ex);
+		// } else {
+		//     outLine("---未授权---");
+		//     s.authorize(function() {
+		//         shareMessage(s, ex);
+		//     }, function(e) {
+		//         outLine("认证授权失败：" + e.code + " - " + e.message);
+		//     });
+		// }
+		// }
+		//
+		// //分享内容
+		// function shareMessage(s,ex){
+		// var msg={
+		// 	title:wxconfig.title,
+		// 	content:wxconfig.description,
+		// 	herf:wxconfig.link,
+		// 	pictures:wxconfig.imgUrl,
+		// 	extra:{scene:ex},
+		// 	};
+		// // if(pic&&pic.realUrl){
+		// //     msg.pictures=[pic.realUrl];
+		// // }
+		// s.send( msg, function(){
+		//     alert( "分享到\""+s.description+"\"成功！ " );
+		// }, function(e){
+		//     alert( "分享到\""+s.description+"\"失败: "+e.code+" - "+e.message );
+		// } );
+		// }
+		// //分享按钮点击事件
+		//
+	 //
+	 // //取消分享
+	 // function cancelAuth(){try{
+	 //
+	 // for ( var i in shares ) {
+	 //     var s = shares[i];
+	 //     if ( s.authenticated ) {
+	 //         outLine( "取消\""+s.description+"\"");
+	 //     }
+	 //     s.forbid();
+	 // }
+	 // // 取消授权后需要更新服务列表
+	 // updateServices();
+	 // outLine( "操作成功！" );}catch(e){alert(e);}
+	 // }
 	 
-	 //取消分享
-	 function cancelAuth(){try{  
-	 
-	 for ( var i in shares ) {  
-	     var s = shares[i];  
-	     if ( s.authenticated ) {  
-	         outLine( "取消\""+s.description+"\"");  
-	     }  
-	     s.forbid();  
-	 }  
-	 // 取消授权后需要更新服务列表  
-	 updateServices();  
-	 outLine( "操作成功！" );}catch(e){alert(e);}  
-	 }
-	 
-	 });// ready end
+	 // });// ready end
 
 	// document.getElementById("share").addEventListener("click", function() {
 	// 	if(navigator.userAgent.indexOf("Html5Plus") > -1) {
@@ -457,4 +457,64 @@ $(function() {
 	// 		//原有wap分享实现
 	// 	}
 	// });// click end
+
+		function openShare() {
+			shareWebview();
+		}
+	mui.plusReady(function() {
+		ws = plus.webview.currentWebview();
+		//关闭splash页面；
+		plus.navigator.closeSplashscreen();
+	});
+	var sharew;
+	var ws = null;
+	/**
+	 *分享窗口
+	 */
+	function shareWebview() {
+		ws = plus.webview.currentWebview();
+		if (sharew) { // 避免快速多次点击创建多个窗口
+			return;
+		}
+		var top = plus.display.resolutionHeight - 134;
+		var href = "share.html";
+		sharew = plus.webview.create(href, "{#getUrlPath service=house template=tt#}", {
+			width: '100%',
+			height: '134',
+			top: top,
+			scrollIndicator: 'none',
+			scalable: false,
+			popGesture: 'none'
+		}, {
+			shareInfo: {
+				"href": wxconfig.link,
+				"title": wxconfig.title,
+				"content": wxconfig.description,
+				// "pageSourceId": ws.id
+			}
+		});
+		sharew.addEventListener("loaded", function() {
+			sharew.show('slide-in-bottom', 300);
+		}, false);
+		// 显示遮罩层
+		ws.setStyle({
+			mask: "rgba(0,0,0,0.5)"
+		});
+		// 点击关闭遮罩层
+		ws.addEventListener("maskClick", closeMask, false);
+	}
+
+	function closeMask() {
+		ws.setStyle({
+			mask: "none"
+		});
+		//避免出现特殊情况，确保分享页面在初始化时关闭
+		if (!sharew) {
+			sharew = plus.webview.getWebviewById("share.html");
+		}
+		if (sharew) {
+			sharew.close();
+			sharew = null;
+		}
+	}
 });
