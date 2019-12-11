a36e5f84b16a3065a70048594b93a4417cacc46c
2643277978 <2643277978@qq.com>
2019-12-11 16:43:05 +0800

    ddd

--- design/pageconfig.js
+++
@@ -1,6 +0,0 @@
-sea.config({
-    base: "/design",
-    alias: {
-        jquery: "lib/jquery/1.10/jquery"
-    }
-});
--- design/widgets/map/view.js
+++
@@ -1,6 +0,0 @@
-define(function(require) {
-    var i = require("ui/imgmax");
-    return function(n) {
-        i(n.find("img"))
-    }
-});
